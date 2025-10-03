a7a38c7c97b23caa050227fb8c94a31369472ec2
ZakharEliseev <eliseev.zv@gmail.com>
2025-10-03 16:03:46 +0500

    refactor renderTask method

--- src/scripts/uiManager.ts
+++ src/scripts/uiManager.ts
@@ -7,21 +7,22 @@ export class UiManager {
     this.todoList = document.querySelector('.todo-list') as HTMLUListElement;
   }
 
-  renderTask(taskData: Array<Task>): void {
-    const li: HTMLLIElement = document.createElement('li');
+  renderTask(list: Array<Task>): void {
 
-    const descr: HTMLParagraphElement = document.createElement('p');
-    descr.textContent = taskData[taskData.length - 1].value;
+    for (let i = 0; i < list.length; i++){
+      const li: HTMLLIElement = document.createElement('li');
+      const descr: HTMLParagraphElement = document.createElement('p');
+      descr.textContent = list[i].value;
+      const completeBtn: HTMLButtonElement = document.createElement('button');
+      completeBtn.textContent = 'Complete';
+      completeBtn.classList.add('todo-list__complete-btn');
 
-    const completeBtn: HTMLButtonElement = document.createElement('button');
-    completeBtn.textContent = 'Complete';
-    completeBtn.classList.add('todo-list__complete-btn');
+      const deleteBtn: HTMLButtonElement = document.createElement('button');
+      deleteBtn.textContent = 'Delete';
+      deleteBtn.classList.add('todo-list__delete-btn');
 
-    const deleteBtn: HTMLButtonElement = document.createElement('button');
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.classList.add('todo-list__delete-btn');
-
-    this.todoList.append(li);
-    li.append(completeBtn, descr, deleteBtn);
+      this.todoList.append(li);
+      li.append(completeBtn, descr, deleteBtn);
+    }
   }
 }
