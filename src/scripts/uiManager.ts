6069b583c609e211a8f9172c9b13247ebb041b15
ZakharEliseev <eliseev.zv@gmail.com>
2025-10-07 18:00:15 +0500

    refactoe

--- src/scripts/uiManager.ts
+++ src/scripts/uiManager.ts
@@ -11,27 +11,27 @@ export class UiManager {
     const button: HTMLButtonElement = document.createElement('button');
     button.textContent = title;
     button.classList.add(`todo-list__${title}Btn`);
-    button.addEventListener('click', () => callback);
+    button.addEventListener('click', callback);
     return button;
   }
 
   render(
-    list: Array<Task>, del: (taskId: number) => void, complete: (taskId: number) => void): void {
+    list: Array<Task>, remove: (taskId: number) => void, complete: (taskId: number) => void): void {
     const fragment: DocumentFragment = new DocumentFragment();
     list.forEach((t) => {
       const li: HTMLLIElement = document.createElement('li');
-      li.dataset.taskId = `${t.id}`;
 
       const text: HTMLParagraphElement = document.createElement('p');
       text.textContent = t.value;
       if (t.isCompleted === true) text.classList.add('todo-list__complete-task');
 
-      const deleteBtn = this.getButton('Delete', () => del(t.id));
+      const deleteBtn = this.getButton('Delete', () => remove(t.id));
       const completeBtn = this.getButton('Complete', () => complete(t.id));
 
       li.append(deleteBtn, text, completeBtn);
       fragment.append(li);
     });
+
     this.todoList.replaceChildren();
     this.todoList.append(fragment);
   }
