4ed7b9cc83bf2b83efd18e412b11f72e364aa205
ZakharEliseev <eliseev.zv@gmail.com>
2025-10-07 17:51:42 +0500

    refactoe

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,24 +2,22 @@ import { Task } from './task';
 import { TaskManager } from './taskManager';
 import { UiManager } from './uiManager';
 
-class App {
+export class App {
   private form: HTMLFormElement;
   private taskManager = new TaskManager();
-  private uiManager: UiManager;
+  private uiManager = new UiManager();
 
   constructor() {
     this.form = document.querySelector('.todo-form') as HTMLFormElement;
-    this.uiManager = new UiManager(this.taskManager);
   }
 
   init(): void {
-    this.uiManager.eventDelete();
-    this.uiManager.eventComplete();
     this.form.addEventListener('submit', (e): void => {
       e.preventDefault();
       this.taskManager.createTask();
       const tasks: Array<Task> = this.taskManager.getTasks();
-      this.uiManager.render(tasks);
+      this.uiManager.render(
+        tasks, this.taskManager.deleteTask, this.taskManager.toggleComplete);
       this.updateUi();
     });
   }
--- src/scripts/uiManager.ts
+++ src/scripts/uiManager.ts
@@ -1,23 +1,22 @@
-
 import { Task } from './task';
-import { TaskManager } from './taskManager';
 
 export class UiManager {
   todoList: HTMLUListElement;
-  private taskManager: TaskManager;
 
-  constructor(taskManager: TaskManager) {
+  constructor() {
     this.todoList = document.querySelector('.todo-list') as HTMLUListElement;
-    this.taskManager = taskManager;
   }
 
-  getButton(title: string): HTMLButtonElement {
+  getButton(title: string, callback: () => void): HTMLButtonElement {
     const button: HTMLButtonElement = document.createElement('button');
     button.textContent = title;
+    button.classList.add(`todo-list__${title}Btn`);
+    button.addEventListener('click', () => callback);
     return button;
   }
 
-  render(list: Array<Task>): void {
+  render(
+    list: Array<Task>, del: (taskId: number) => void, complete: (taskId: number) => void): void {
     const fragment: DocumentFragment = new DocumentFragment();
     list.forEach((t) => {
       const li: HTMLLIElement = document.createElement('li');
@@ -27,40 +26,13 @@ export class UiManager {
       text.textContent = t.value;
       if (t.isCompleted === true) text.classList.add('todo-list__complete-task');
 
-      const deleteBtn = this.getButton('Delete');
-      deleteBtn.classList.add('todo-list__deleteBtn');
-
-      const completeBtn = this.getButton('Complete');
-      completeBtn.classList.add('todo-list__completeBtn');
+      const deleteBtn = this.getButton('Delete', () => del(t.id));
+      const completeBtn = this.getButton('Complete', () => complete(t.id));
 
       li.append(deleteBtn, text, completeBtn);
       fragment.append(li);
     });
     this.todoList.replaceChildren();
     this.todoList.append(fragment);
-  }
-
-  eventDelete() {
-    this.todoList.addEventListener('click', (e) => {
-      if ((e.target as Element).closest('.todo-list__deleteBtn')) {
-        const currentLi = (e.target as Element).closest('li') as HTMLLIElement;
-        const taskId = Number(currentLi.dataset.taskId);
-        
-        this.taskManager.deleteTask(taskId)
-        this.render(this.taskManager.getTasks());
-      }
-    });
-  }
-
-  eventComplete() {
-    this.todoList.addEventListener('click', (e) => {
-      if ((e.target as Element).closest('.todo-list__completeBtn')) {
-        const currentLi = (e.target as Element).closest('li') as HTMLLIElement;
-        const taskId = Number(currentLi.dataset.taskId);
-        
-        this.taskManager.toggleComplete(taskId);
-        this.render(this.taskManager.getTasks());
-      }
-    });
   }
 }
